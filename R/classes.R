46ee73aa4ebd44f5434d9b7151f3d44cd2b5af94
Duncan Temple Lang <duncan@r-project.org>
2016-08-24 18:03:29 -0700

    manually specified classes for the primitive types in poppler - Goffset, GooString, GBool

--- R/classes.R
+++ R/classes.R
@@ -1,11 +1,15 @@
+if(FALSE) {
 setClass("PDFDoc", contains = "RC++Reference")
-
 setClass("GfxState", contains = "RC++Reference")
 setClass("GfxStatePtr", contains = "GfxState") # XXX Fix
-
 setClass("XRef", contains = "RC++Reference")
 setClass("XRefPtr", contains = "XRef") # XXX Fix
+}
 
+setClass("GBool", contains = "logical")
+setClass("Goffset", contains = "numeric")
+
+setClassUnion("GooStringOrCharacter", c("GooString", "character", "GooStringPtr"))
 
 setClass("ROutputDev", contains = "RC++Reference")
 
