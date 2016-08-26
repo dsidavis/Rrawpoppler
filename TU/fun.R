c61cd72666d85773b89c91f74f0be2a269461b7b
Duncan Temple Lang <duncan@r-project.org>
2016-08-25 18:47:18 -0700

    moved the gen() function to a separate file so we can reuse it

--- TU/fun.R
+++ TU/fun.R
@@ -1,17 +1,7 @@
 library(RCIndex)
 library(RCodeGen)
-gen = 
-function()
-{
-    ans = list()
-    v = function(cur, ...) {
-         if(cur$kind == CXCursor_ClassDecl)
-            ans[[ length(ans) + 1]]  <<- cur
-        
-        CXChildVisit_Continue
-    }
-    list(visitor = v, ans = function() ans)
-}
+source("readClasses.R")
+
 
 if(TRUE) {
 #source("fun.R")
