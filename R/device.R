492b79a14b351c170e098bac29f824777a5e0b89
Duncan Temple Lang <duncan@r-project.org>
2016-08-29 10:37:17 -0700

    constructor function moved from displayPages.R. Experimented with having a generic C++ routine for set/get functions for RFunctionsNativeMethods, but doesn't work, as anticipated.  Need to have the ROutputDev not cast it to the second base class

--- R/device.R
+++ R/device.R
@@ -1,20 +1,52 @@
+ROutputDev = ROutputDevice =
+function(..., .funs = list(...))
+{
+    if(length(.funs) == 1  && is.list(.funs[[1]]) && all(sapply(.funs[[1]], is.function)))
+        .funs = .funs[[1]]
+    
+    funs = ROutputFunctions(.funs = .funs)
+    .Call("R_ROutputDev_new", funs)
+}
+
+DefaultDeviceFunctions =
+    list(upsideDown = function(){ message("in upsideDown") ;  TRUE},
+         useDrawChar = function() TRUE,
+         interpretType3Chars = function() FALSE)
+
+
+ROutputFunctions =
+function(..., .funs = list(...), defaults = DefaultDeviceFunctions)
+{
+ #XXX Check the names.    
+   defaults[names(.funs)] = .funs
+   defaults
+}
+
+
+
+
+
 setGeneric("setMethods",     # very close to S4 setMethod()
            function(x, methods, ...)
                standardGeneric("setMethods"))
-
-setMethod("setMethods", "ROutputDev",
-          function(x, methods, protect = TRUE, ...) {
-              .Call("R_ROutputDev_setFunctions", x, methods, as.logical(protect))
-          })
-
 
 setGeneric("getMethods",
             function(x, ...)
               standardGeneric("getMethods"))
 
+
+setMethod("setMethods", "ROutputDev",
+          function(x, methods, protect = TRUE, ...) {
+              .Call("R_ROutputDev_setFunctions", x, methods, as.logical(protect))
+#              .Call("R_RFunctionsNativeMethods_setFunctions", x, methods, as.logical(protect))              
+          })
+
+
 setMethod("getMethods", "ROutputDev",
             function(x, ...)
-               .Call("R_ROutputDev_getFunctions", x))
+               .Call("R_ROutputDev_getFunctions", x)
+#               .Call("R_RFunctionsNativeMethods_getFunctions", x)          
+          )
     
 
 
