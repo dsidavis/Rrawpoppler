71330693801c5e1ab40251988102d40dff5d3ef8
Duncan Temple Lang <duncan@r-project.org>
2016-08-29 11:51:27 -0700

    removed manually written routines

--- src/ROutputDev.cc
+++ src/ROutputDev.cc
@@ -9,7 +9,10 @@ RFunctionsNativeMethods::lookupRMethod(const char *name)
     int n = Rf_length(r_method_funcs);
     for(int i = 0; i < n; i++) {
         if(strcmp(CHAR(STRING_ELT(names, i)), name) == 0) {
+#ifdef R_DEBUG_DEVICE
 //            Rprintf("Found R function for method %s\n", name);
+//            Rf_PrintValue(VECTOR_ELT(r_method_funcs, i));
+#endif
             return(VECTOR_ELT(r_method_funcs, i));
         }
     }
@@ -17,6 +20,7 @@ RFunctionsNativeMethods::lookupRMethod(const char *name)
 }
 
 
+#if 1
 extern "C"
 SEXP
 R_ROutputDev_setFunctions(SEXP r_dev, SEXP r_funs, SEXP r_preserve)
@@ -32,106 +36,26 @@ R_ROutputDev_getFunctions(SEXP r_dev)
     ROutputDev *dev = GET_REF(r_dev, ROutputDev);
     return(dev->getFunctions());
 }
+#else
 
-
-
-#if 0
-GBool 
-ROutputDev::upsideDown()
+extern "C"
+SEXP
+R_RFunctionsNativeMethods_setFunctions(SEXP r_dev, SEXP r_funs, SEXP r_preserve)
 {
-    SEXP fun = lookupRMethod("upsideDown");
-    if(fun == R_NilValue) 
-        return(1);
-    
-    SEXP e;
-    PROTECT(e = allocVector(LANGSXP, 1));
-    SETCAR(e, fun);
-    SEXP r_ans = invokeMethod(e);
-    UNPROTECT(1);
-    return((GBool) LOGICAL(r_ans)[0]);
-}
-GBool 
-ROutputDev::useDrawChar()
-{
-    SEXP fun = lookupRMethod("useDrawChar");
-    if(fun == R_NilValue) 
-        return(1);
-    
-    SEXP e;
-    PROTECT(e = allocVector(LANGSXP, 1));
-    SETCAR(e, fun);
-    SEXP r_ans = invokeMethod(e);
-    UNPROTECT(1);
-    return((GBool) LOGICAL(r_ans)[0]);
+    RFunctionsNativeMethods *dev = GET_REF(r_dev, RFunctionsNativeMethods);
+    return(dev->setFunctions(r_funs, LOGICAL(r_preserve)[0]));
 }
 
-GBool 
-ROutputDev::interpretType3Chars()
+extern "C"
+SEXP
+R_RFunctionsNativeMethods_getFunctions(SEXP r_dev)
 {
-    SEXP fun = lookupRMethod("interpretType3Chars");
-    if(fun == R_NilValue) 
-        return(0); 
-    
-    SEXP e;
-    PROTECT(e = allocVector(LANGSXP, 1));
-    SETCAR(e, fun);
-    SEXP r_ans = invokeMethod(e);
-    UNPROTECT(1);
-    return((GBool) LOGICAL(r_ans)[0]);
+    RFunctionsNativeMethods *dev = GET_REF(r_dev, RFunctionsNativeMethods);
+    Rf_PrintValue(dev->getFunctions());
+    return(dev->getFunctions());
 }
-
-
-
-void 
-ROutputDev::startPage(int pageNum, GfxState *state, XRef *xref)
-{
-    SEXP fun = lookupRMethod("startPage");
-    if(fun == R_NilValue) 
-        return;
-
-    SEXP e, cur;
-    PROTECT(e = cur = allocVector(LANGSXP, 4));
-    SETCAR(cur, fun); cur = CDR(cur);
-    SETCAR(cur, ScalarInteger(pageNum)); cur = CDR(cur);
-    SETCAR(cur, createRef(state, "GfxState", NULL)); cur = CDR(cur);
-    SETCAR(cur, createRef(xref, "XRef", NULL)); 
-
-    invokeMethod(e);
-    UNPROTECT(1);
-}
-
-void 
-ROutputDev::endPage()
-{
-    SEXP fun = lookupRMethod("endPage");
-    if(fun == R_NilValue) 
-        return;
-
-    SEXP e, cur;
-    PROTECT(e = cur = allocVector(LANGSXP, 1));
-    SETCAR(cur, fun);
-
-    invokeMethod(e);
-    UNPROTECT(1);
-}
-
-
-void 
-ROutputDev::updateFillColor(GfxState * state)
-{
-    SEXP fun = lookupRMethod("updateFillColor");
-    if(fun == R_NilValue) 
-        return;
-
-    SEXP e, cur;
-    PROTECT(e = cur = allocVector(LANGSXP, 2));
-    SETCAR(cur, fun); cur = CDR(cur);
-    SETCAR(cur, createRef(state, "GfxState", NULL)); 
-
-    invokeMethod(e);
-    UNPROTECT(1);
-}
-
 #endif
+
+
 
 #include "ROutputDev_auto.cpp_code"
