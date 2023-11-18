04f71f57cbc7866a6ee89b9b64a3ae251ad93ad8
Duncan Temple Lang <duncan@r-project.org>
2023-11-18 15:10:16 -0800

    ???

--- TU/tu.R
+++ TU/tu.R
@@ -2,5 +2,9 @@ library(RCIndex)
 
 tu = createTU("poppler.cc", includes = c("/usr/local/include/poppler", "/usr/local/include"))
 
-k = getCppClasses(tu)
+k = getCppClasses(tu, "/poppler/", nodesOnly = TRUE)
+k = getCppClasses(tu, "/poppler/")
+#k = getCppClasses(tu)
 names(k)
+
+
